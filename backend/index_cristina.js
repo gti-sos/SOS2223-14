6e808d057e6eb9e75d77398f8e3c3c7d273b8f42
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-03-20 14:12:40 +0100

    Cambios

--- backend/index_cristina.js
+++ backend/index_cristina.js
@@ -118,23 +118,6 @@ module.exports = (app) => {
         }
     ];
 
-    app.get(BASE_API_URL+"/apartment-occupancy-surveys", (request,response) => {
-        console.log("New GET to /apartment-occupancy-surveys");
-        db.insert(datos, (err, data)=>{
-            if(err){
-                console.log(`Error geting /apartment-occupancy-surveys: ${err}`);
-                response.sendStatus(500);
-            }else{
-                console.log(`data inserted: ${datos.length}`);
-                response.json(datos.map((d)=>{
-                    delete d._id;
-                    return d;
-                }));                           
-            }
-        });
-        
-    });
-
     app.get(BASE_API_URL+"/apartment-occupancy-surveys/docs", (request,response) => {
         response.redirect("https://documenter.getpostman.com/view/25998017/2s93Jxqfu4");
     });
@@ -163,30 +146,201 @@ module.exports = (app) => {
         });
     });
 
-    //Usa paginacion
-    app.get(BASE_API_URL+"/apartment-occupancy-surveys/:province", (request,response) => {
-        var ciudad = request.params.province;
+    app.get(BASE_API_URL+"/apartment-occupancy-surveys", (request,response) => {
+        console.log("New GET to /apartment-occupancy-surveys");
 
-        console.log(`New GET to /apartment-occupancy-surveys/${ciudad}`);
+        var province = request.query.province;
+        var year = request.query.year;
+        var from = request.query.from;
+        var to = request.query.to;
 
-        db.find({province : ciudad}).skip(0).limit(datos.length).exec((err, data) =>{
-            if(err){
-                console.log(`Error geting /apartment-occupancy-surveys/${ciudad}: ${err}`);
-                response.sendStatus(500);
-            }else{
-                if(data.length!= 0){
-                    console.log(`data returned ${data.length}`);
-                    response.json(data.map((d)=>{
-                        delete d._id;
-                        return d;
-                    }));
-                }
-                 else{
-                    console.log(`Data not found /apartment-occupancy-surveys/${ciudad}: ${err}`);
-                    response.status(404).send("Data not found");
-                 }        
+        // Comprobamos query
+
+        for(var i = 0; i<Object.keys(request.query).length;i++){
+            var element = Object.keys(request.query)[i];
+            if(element != "year" && element != "from" && element != "to" && element != "province" && element != "limit" && element != "offset"){
+                console.log(`No se han recibido los campos esperados:`);
+                response.status(400).send("Bad Request");
             }
+        }
+
+        // Comprobamos si from es menor o igual a to
+        
+        if(from>to){
+            console.log(`No se han recibido los campos esperados:`);
+            response.status(400).send("Bad Request");
+        }
+
+        // db.find({}).skip(0).limit(5).exec((err, data) =>{ 
+        db.find({},function(err, filteredList){
+            
+            if(err){
+                console.log(`Error geting /apartment-occupancy-surveys: ${err}`);
+                response.sendStatus(500);
+            }
+
+            // Apartado para búsqueda por provincia
+
+            if (province != null){
+                var filteredList = filteredList.filter((reg)=>
+                {
+                    return (reg.province == province);
+                });
+
+                if (filteredList==0){
+                    console.log(`Data not found /apartment-occupancy-surveys: ${err}`);
+                    response.status(404).send("Data not found");
+                }
+            }
+
+            // Apartado para búsqueda por año
+
+            if (year != null){
+                var filteredList = filteredList.filter((reg)=>
+                {
+                    return (reg.year == year);
+                });
+
+                if (filteredList==0){
+                    console.log(`Data not found /apartment-occupancy-surveys: ${err}`);
+                    response.status(404).send("Data not found");
+                }
+            }
+
+            // Apartado para from y to
+            
+            if(from != null && to != null){
+                filteredList = filteredList.filter((reg)=>
+                {
+                    return (reg.year >= from && reg.year <=to);
+                });
+
+                if (filteredList==0){
+                    console.log(`Data not found /apartment-occupancy-surveys: ${err}`);
+                    response.status(404).send("Data not found");
+                }    
+            }
+
+            // Resultado
+
+            if(request.query.limit != undefined || request.query.offset != undefined){
+                filteredList = paginacion(request,filteredList);
+            }
+            
+            filteredList.forEach((element)=>{
+                delete element._id;
+            });
+
+            if(request.query.fields!=null){
+                //Comprobamos si los campos son correctos
+                var listaFields = request.query.fields.split(",");
+                for(var i = 0; i<listaFields.length;i++){
+                    var element = listaFields[i];
+                    if(element != "province" && element != "year" && element != "traveler" && element != "overnight_stay" && element != "average_stay"){
+                        console.log(`No se han recibido los campos esperados:`);
+                        response.status(400).send("Bad Request");
+                    }
+                }
+            }
+            response.send(JSON.stringify(filteredList,null,2));
+
         });
+        
+    });
+
+    
+
+    // GET /apartment-occupancy-surveys/:province
+
+    app.get(BASE_API_URL+"/apartment-occupancy-surveys/:province",(request, response)=>{
+    
+        var province =request.params.province;
+        var from = request.query.from;
+        var to = request.query.to;
+
+        console.log(`New GET to /apartment-occupancy-surveys/${province}`);
+
+        //Comprobamos query
+
+        for(var i = 0; i<Object.keys(request.query).length;i++){
+            var element = Object.keys(request.query)[i];
+            if(element != "from" && element != "to"){
+                console.log(`No se han recibido los campos esperados:`);
+                response.status(400).send("Bad Request");
+            }
+        }
+
+        //Comprobamos si from es mas pequeño o igual a to
+
+        if(from>to){
+            console.log(`No se han recibido los campos esperados:`);
+            response.status(400).send("Bad Request");
+        }
+
+        db.find({}, function(err,filteredList){
+            
+            if(err){
+                console.log(`Error geting /apartment-occupancy-surveys: ${err}`);
+                response.sendStatus(500);
+            }
+
+            filteredList = filteredList.filter((reg)=>
+            {
+                return (reg.province == province);
+            });
+        
+            // Apartado para from y to
+            var from = request.query.from;
+            var to = request.query.to;
+    
+            //Comprobamos si from es mas pequeño o igual a to
+            if(from>to){
+                console.log(`No se han recibido los campos esperados:`);
+                response.status(400).send("Bad Request");
+            }
+        
+            if(from != null && to != null && from<=to){
+                filteredList = filteredList.filter((reg)=>
+                {
+                   return (reg.year >= from && reg.year <=to);
+                }); 
+                
+            }
+            //Comprobamos si existe 
+
+            if (filteredList==0){
+                console.log(`Data not found /apartment-occupancy-surveys: ${err}`);
+                    response.status(404).send("Data not found");
+            }
+
+            //Resultado
+
+            if(request.query.limit != undefined || request.query.offset != undefined){
+                filteredList = paginacion(request,filteredList);
+            }
+            filteredList.forEach((element)=>{
+                delete element._id;
+            });
+
+            //Comprobamos fields
+
+            if(request.query.fields!=null){
+
+                //Comprobamos si los campos son correctos
+
+                var listaFields = request.query.fields.split(",");
+                for(var i = 0; i<listaFields.length;i++){
+                    var element = listaFields[i];
+                    if(element != "province" && element != "year" && element != "traveler" && element != "overnight_stay" && element != "average_stay"){
+                        console.log(`No se han recibido los campos esperados:`);
+                        response.status(400).send("Bad Request");
+                    }
+                }
+            }
+
+            response.send(JSON.stringify(filteredList,null,2));
+        })
+
     });
 
     app.get(BASE_API_URL+"/apartment-occupancy-surveys//:year", (request,response) => {
@@ -441,4 +595,21 @@ module.exports = (app) => {
             }
         });
     });   
+};
+
+
+function paginacion(req, lista){
+
+    var res = [];
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    
+    if(limit < 1 || offset < 0 || offset > lista.length){
+        res.push("ERROR EN PARAMETROS LIMIT Y/O OFFSET");
+        return res;
+    }
+
+    res = lista.slice(offset,parseInt(limit)+parseInt(offset));
+    return res;
+
 };
