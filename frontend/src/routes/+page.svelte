fea6ff9b491ff9228ea05890e736a6b2a4766fa0
Cristina Calderon Garcia <124584828+criscalgar@users.noreply.github.com>
2023-04-10 13:52:44 +0200

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,23 @@
 <main>
     <h1><u>SOS2223-14</u></h1>
-    <h2>Project description</h2>
+    <h2>Descripción del proyecto</h2>
     <p style="font-size: 20px;">
-        Our project consists of analyzing the occupancy in tourist apartments,
-        the tourism situation and hotel occupancy in Andalusia.
+        Nuestro proyecto consiste en analizar la ocupación en apartamentos turísticos,
+        la situación del turismo y la ocupación hotelera en Andalucía
     </p>
-    <h2>Team</h2>
+    <h2>Equipo</h2>
     <ul>
         <li>
-            <a href="https://github.com/criscalgar">Cristina Calderón García</a>
+            <a href="https://github.com/criscalgar">Cristina Calderón García (Encuesta de ocupación de apartamentos)</a>
         </li>
         <li>
-            <a href="https://github.com/rebsuabas">Rebeca Suárez Bascón</a>
+            <a href="https://github.com/rebsuabas">Rebeca Suárez Bascón(Encuesta de situación turística de Andalucía)</a>
         </li>
         <li>
             <a href="https://github.com/anachavel1">Ana Chávez Velasco</a>
         </li>
     </ul>
-    <h2>Repository</h2>
+    <h2>Repositorio</h2>
     <a href="https://github.com/gti-sos/SOS2223-14" class="margen"
         >gti-sos/SOS2223-14</a
     >
@@ -25,95 +25,75 @@
     <a href="https://sos2223-14.appspot.com" class="margen"
         >https://sos2223-14.appspot.com</a
     >
-    <h2>PostMan Documentation</h2>
+    <h2>Documentación PostMan</h2>
     <ul>
-        <a
-            href="https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys/docs"
-        >
+        <a href="https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys/docs">
             https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys/docs
         </a>
-        <br />
-        <a
-            href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys/docs"
-        >
+        <br/>
+        <a href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys/docs">
             https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys/docs
         </a>
-        (developed by Rebeca Suárez Bascón)
+        (desarrollado por Rebeca Suárez Bascón)
     </ul>
     <ul>
-        <a
-            href="https://documenter.getpostman.com/view/25998017/2s93Jxqfu4"
-        >
+        <a href="https://documenter.getpostman.com/view/25998017/2s93Jxqfu4">
             https://sos2223-14.appspot.com/api/v1/apartment-occupancy-surveys/docs
         </a>
-        <br />
-        <a
-            href="https://documenter.getpostman.com/view/25998017/2s93XsX5xc"
-        >
+        <br/>
+        <a href="https://documenter.getpostman.com/view/25998017/2s93XsX5xc">
             https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys/docs
         </a>
-        (developed by Cristina Calderón García)
+        (desarrollado por Cristina Calderón García)
     </ul>
     <ul>
-        <a
-            href="https://sos2223-14.appspot.com/api/v1/hotel-occupancy-surveys/docs"
-        >
+        <a href="https://sos2223-14.appspot.com/api/v1/hotel-occupancy-surveys/docs">
             https://sos2223-14.appspot.com/api/v1/hotel-occupancy-surveys/docs
         </a>
-        <br />
-        <a
-            href="https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys/docs"
-        >
+        <br/>
+        <a href="https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys/docs">
             https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys/docs
         </a>
-        (developed by Ana Chávez Velasco)
+        (desarrollado por Ana Chávez Velasco)
     </ul>
     <h2>APIs</h2>
     <ul>
-        <a
-            href="https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys"
-        >
+        <a href="https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys">
             https://sos2223-14.appspot.com/api/v1/andalusia-tourism-situation-surveys
         </a>
-        <br />
-        <a
-            href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys"
-        >
+        <br/>
+        <a href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys">
             https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys
         </a>
-        (developed by Rebeca Suárez Bascón)
+        (desarrollado por Rebeca Suárez Bascón)
     </ul>
     <ul>
-        <a
-            href="https://sos2223-14.appspot.com/api/v1/apartment-occupancy-surveys"
-        >
+        <a href="https://sos2223-14.appspot.com/api/v1/apartment-occupancy-surveys">
             https://sos2223-14.appspot.com/api/v1/apartment-occupancy-surveys
         </a>
-        <br />
-        <a
-            href="http://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys"
-        >
+        <br/>
+        <a href="http://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys">
             https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys
         </a>
-        (developed by Cristina Calderón García)
+        (desarrollado por Cristina Calderón García)
     </ul>
     <ul>
         <a href="https://sos2223-14.appspot.com/api/v1/hotel-occupancy-surveys">
             https://sos2223-14.appspot.com/api/v1/hotel-occupancy-surveys
         </a>
-        <br />
+        <br/>
         <a href="https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys">
             https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys
         </a>
-        (developed by Ana Chávez Velasco)
+        (desarrollado por Ana Chávez Velasco)
     </ul>
     <div id="final">
         <h2>FrontEnd</h2>
-        <a
-            href="http://localhost:12345/apartment-occupancy-surveys"
-            class="margen"
-        >
-            FrontEnd -> Apartment occupancy surveys
+        <a href="http://localhost:12345/apartment-occupancy-surveys" class="margen">
+            FrontEnd -> Encuesta de ocupacion de apartamentos
+        </a>
+        <a href="http://localhost:12345/andalusia-tourism-situation-surveys" class="margen">
+            FrontEnd -> Encuesta de situacion turistica de Andalucía
         </a>
     </div>
 </main>
