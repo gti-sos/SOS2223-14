79d07665e37c5c40fd71cae87cd02412742979c9
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-04-26 17:47:36 +0200

    Cambios

--- frontend/src/routes/integrations/apartment-occupancy-surveys/graficas/+page.svelte
+++ frontend/src/routes/integrations/apartment-occupancy-surveys/graficas/+page.svelte
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="photo-block">
             <div class="photo-image-block">
-                <a href="http://sos2223-14.ew.r.appspot.com/integrations/apartment-occupancy-surveys/withProxy"><img src = "https://3.bp.blogspot.com/-X24aRlnk89Q/Vg6xCpICQEI/AAAAAAAAFAU/Pqm7ua8Cm5k/s1600/estadistica2.jpg" alt="Imagen"></a>
+                <a href="http://sos2223-14.ew.r.appspot.com/integrations/apartment-occupancy-surveys/graficas/withProxy"><img src = "https://3.bp.blogspot.com/-X24aRlnk89Q/Vg6xCpICQEI/AAAAAAAAFAU/Pqm7ua8Cm5k/s1600/estadistica2.jpg" alt="Imagen"></a>
             </div>
             <div class="photo-metadata-block">
                 <h2 class="photo-title">Con proxy</h2>
@@ -10,7 +10,7 @@
         </div>
         <div class="photo-block" style="margin-left: 20px;">
             <div class="photo-image-block">
-                <a href="http://sos2223-14.ew.r.appspot.com/integrations/apartment-occupancy-surveys/withoutProxy"><img src = "https://thumbs.dreamstime.com/b/gr%C3%A1ficos-financieros-41675922.jpg" alt="Imagen"></a>
+                <a href="http://sos2223-14.ew.r.appspot.com/integrations/apartment-occupancy-surveys/graficas/withoutProxy"><img src = "https://thumbs.dreamstime.com/b/gr%C3%A1ficos-financieros-41675922.jpg" alt="Imagen"></a>
             </div>
             <div class="photo-metadata-block">
                 <h2 class="photo-title">Sin proxy</h2>
