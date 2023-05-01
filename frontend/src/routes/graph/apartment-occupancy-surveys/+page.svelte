199e6f46fdcf95f87ffa33408506b6972841d668
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-01 15:41:15 +0200

    Cambios

--- frontend/src/routes/graph/apartment-occupancy-surveys/+page.svelte
+++ frontend/src/routes/graph/apartment-occupancy-surveys/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     //@ts-nocheck
     import { onMount } from "svelte";
-    import {Table, Button, Alert} from "sveltestrap";
+    import { Table, Button, Alert } from "sveltestrap";
 
     const colors = [
         "primary",
@@ -14,29 +14,35 @@
         "dark",
     ];
 
-    let API = "https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys";
+    let API =
+        "https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys";
     let p = "";
-    let dato="";
-    let datos="";
-    let provincias=[];
-    let traveler=[];
-    let overnight_stay=[]
-    let average_stay=[];
-    let province="";
-    let result="";
-    let resultStatus="";
-    let message="";
+    let dato = "";
+    let datos = "";
+    let provincias = [];
+    let traveler = [];
+    let overnight_stay = [];
+    let average_stay = [];
+    let province = "";
+    let result = "";
+    let resultStatus = "";
+    let message = "";
     let c = "";
 
-    let traveler_js=[];
-    let overnight_stay_js=[]
-    let average_stay_js=[];
+    let traveler_js = [];
+    let overnight_stay_js = [];
+    let average_stay_js = [];
 
     onMount(async () => {
         getAllData();
     });
 
-    async function loadHighCharts(provincias,traveler,overnight_stay,average_stay) {
+    async function loadHighCharts(
+        provincias,
+        traveler,
+        overnight_stay,
+        average_stay
+    ) {
         Highcharts.chart("container", {
             chart: {
                 type: "column",
@@ -105,56 +111,55 @@
                 overnight_stay.push(parseInt(datos[i]["overnight_stay"]));
                 average_stay.push(parseInt(datos[i]["average_stay"]));
             }
-            loadHighCharts(
-                provincias,
-                traveler,
-                overnight_stay,
-                average_stay
-            );
+            loadHighCharts(provincias, traveler, overnight_stay, average_stay);
         } catch (error) {
             console.log(`Error parsing result: ${error}`);
         }
     }
 
-    async function loadJSCharting(traveler_js,overnight_stay_js,average_stay_js) {
-        var chart = JSC.chart('chartDiv', { 
-            debug: true, 
-            defaultSeries_type: 'column', 
-            title_label_text: '', 
-            yAxis: { label_text: ''}, 
+    async function loadJSCharting(
+        traveler_js,
+        overnight_stay_js,
+        average_stay_js
+    ) {
+        var chart = JSC.chart("chartDiv", {
+            debug: true,
+            defaultSeries_type: "column",
+            title_label_text: "",
+            yAxis: { label_text: "" },
             xAxis: {
-                label_text: '', 
-                categories: ['']
-            }, 
-            series: [ 
-                { 
-                name: 'Turistas',  
-                points: traveler_js
-                }, 
-                { 
-                name: 'Pernoctaciones', 
-                points: overnight_stay_js
-                }, 
-                { 
-                name: 'Estancia media', 
-                points: average_stay_js
-                }, 
-            ] 
+                label_text: "",
+                categories: [""],
+            },
+            series: [
+                {
+                    name: "Turistas",
+                    points: traveler_js,
+                },
+                {
+                    name: "Pernoctaciones",
+                    points: overnight_stay_js,
+                },
+                {
+                    name: "Estancia media",
+                    points: average_stay_js,
+                },
+            ],
         });
     }
 
     async function getData(province) {
         resultStatus = result = "";
-        const res = await fetch(API + "/"+province, {
+        const res = await fetch(API + "/" + province, {
             method: "GET",
         });
         try {
             const data = await res.json();
             dato = data;
-            traveler_js=[];
-            overnight_stay_js=[]
-            average_stay_js=[];
-            for(let i=0; i<dato.length; i++){
+            traveler_js = [];
+            overnight_stay_js = [];
+            average_stay_js = [];
+            for (let i = 0; i < dato.length; i++) {
                 traveler_js.push(dato[i]["traveler"]);
                 overnight_stay_js.push(dato[i]["overnight_stay"]);
                 average_stay_js.push(dato[i]["average_stay"]);
@@ -167,16 +172,15 @@
         if (status == 200) {
             message = "Provincia encontrada";
             c = "success";
-            loadJSCharting(traveler_js,overnight_stay_js,average_stay_js);
+            loadJSCharting(traveler_js, overnight_stay_js, average_stay_js);
         } else if (status == 404) {
             message = "Provincia no encontrada";
             c = "danger";
-        }else if (status == 500) {
+        } else if (status == 500) {
             message = "Error interno";
             c = "danger";
         }
     }
-
 </script>
 
 <svelte:head>
@@ -190,25 +194,55 @@
     {#if message != ""}
         <Alert color={c}>{message}</Alert>
     {/if}
-    <h1 style="text-align:center;font-style: oblique; font-family: initial;">Gráfica ocupación de apartamentos</h1>
-    <p style="text-align:center">Gráfica con: <a style="text-decoration: none; color:black" href="https://www.highcharts.com/" target="_blank"><u>Highcharts</u></a></p>
+    <h1 style="text-align:center;font-style: oblique; font-family: initial;">
+        Gráfica ocupación de apartamentos
+    </h1>
+    <p style="text-align:center">
+        Gráfica con: <a
+            style="text-decoration: none; color:black"
+            href="https://www.highcharts.com/"
+            target="_blank"><u>Highcharts</u></a
+        >
+    </p>
     <figure class="highcharts-figure">
-        <div id="container"/>
+        <div id="container" />
         <p class="highcharts-description" />
     </figure>
-    <h2 style="text-align: center; font-style: oblique; font-family: initial;">Gráfica {province}</h2>
-    <p style="text-align:center">Gráfica con: <a style="text-decoration: none; color:black" href="https://jscharting.com/" target="_blank"><u>JSCharting</u></a></p>
-    <h3 style="text-align: center; font-style: oblique;"><u>Introduce una ciudad para mostrar su gráfica</u></h3>
+    <h2 style="text-align: center; font-style: oblique; font-family: initial;">
+        Gráfica {province}
+    </h2>
+    <p style="text-align:center">
+        Gráfica con: <a
+            style="text-decoration: none; color:black"
+            href="https://jscharting.com/"
+            target="_blank"><u>JSCharting</u></a
+        >
+    </p>
+    <h3 style="text-align: center; font-style: oblique;">
+        <u>Introduce una ciudad para mostrar su gráfica</u>
+    </h3>
     <Table>
         <tbody>
             <tr>
-                <td><input style="margin-left: 800px; height: 45px; text-align: center;" placeholder="Provincia" bind:value={province} /></td>
                 <td>
-                    <Button style="margin-right: 800px; height: 45px;" color="warning" on:click={getData(province)}>Buscar</Button>
+                    <input
+                        style="margin-left: 400px;height: 45px; text-align: center;"
+                        placeholder="Provincia"
+                        bind:value={province}
+                    />
+                </td>
+                <td>
+                    <Button
+                        style="height: 45px;"
+                        color="warning"
+                        on:click={getData(province)}>Buscar</Button
+                    >
                 </td>
             </tr>
         </tbody>
     </Table>
-    <div id="chartDiv" style="max-width:740px; height:400px; margin: 0px auto; margin-top:40px">
-    </div>
+    <div
+        id="chartDiv"
+        style="max-width:740px; height:400px; margin: 0px auto; margin-top:40px"
+    />
 </main>
