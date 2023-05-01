0b0789ca5b1764580397f681e0d5f70346f59ed0
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-01 17:09:01 +0200

    Cambios

--- frontend/src/routes/graph/group/+page.svelte
+++ frontend/src/routes/graph/group/+page.svelte
@@ -80,7 +80,7 @@
                 if (!provincias.includes(p)) {
                     provincias.push(p);
                 }
-                
+
                 average_daily_expenditure.push(
                     parseFloat(datos[i]["average_daily_expenditure"])
                 );
@@ -116,7 +116,7 @@
                 );
             }
 
-            loadHighCharts(
+            loadJSCharting(
                 provincias,
                 traveler,
                 overnight_stay,
@@ -132,7 +132,8 @@
             console.log(`Error parsing result: ${error}`);
         }
     }
-    async function loadHighCharts(
+
+    async function loadJSCharting(
         provincias,
         traveler,
         overnight_stay,
@@ -144,113 +145,40 @@
         occupancy_rate_by_weekend_place,
         room_occupancy_rate
     ) {
-        Highcharts.chart("container", {
-            chart: {
-                type: "column",
-            },
-            title: {
-                text: "",
-            },
-            subtitle: {
-                text: 'Source: <a href="https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys" target="_blank">API Ocupación de apartamentos en Andalucía</a>, <a href="https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys" target="_blank">API Ocupación de hoteles en Andalucía</a> y <a href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys" target="_blank">API Coyuntura turística en Andalucía</a>',
-            },
+        var chart = JSC.chart("chartDiv", {
+            debug: true,
+            defaultSeries_type: "column",
+
+            title_label_text: "",
+            yAxis: { label_text: "" },
             xAxis: {
+                label_text: "",
                 categories: provincias,
-                crosshair: true,
-            },
-            yAxis: {
-                min: 0,
-                max: 10,
-                title: {
-                    text: "Total",
-                },
-            },
-            tooltip: {
-                headerFormat:
-                    '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat:
-                    '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.f}</b></td></tr>',
-                footerFormat: "</table>",
-                shared: true,
-                useHTML: true,
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0,
-                },
             },
             series: [
                 {
                     name: "Turistas",
-                    data: traveler,
+                    points: traveler,
                 },
                 {
                     name: "Pernoctaciones",
-                    data: overnight_stay,
+                    points: overnight_stay,
                 },
                 {
                     name: "Estancia media",
-                    data: average_stay,
+                    points: average_stay,
                 },
                 {
                     name: "Gasto medio",
-                    data: average_daily_expenditure,
-                },
+                    points: average_daily_expenditure,
+                },                
                 {
                     name: "Media de empleo",
-                    data: average_employment,
+                    points: average_employment,
                 },
                 {
                     name: "Habitaciones",
-                    data: rooms,
-                },
-                {
-                    name: "Tasa de ocupación por lugar",
-                    data: occupancy_rate_by_place,
-                },
-                {
-                    name: "Tasa de ocupación por lugar de fin de semana",
-                    data: occupancy_rate_by_weekend_place,
-                },
-                {
-                    name: "Tasa de ocupacion por habitación",
-                    data: room_occupancy_rate,
-                },
-            ],
-        });
-    }
-
-    async function loadJSCharting(
-        provincias,
-        traveler,
-        overnight_stay,
-        average_stay,
-        average_daily_expenditure,
-        rooms,
-        occupancy_rate_by_place,
-        occupancy_rate_by_weekend_place,
-        room_occupancy_rate
-    ) {
-        var chart = JSC.chart("chartDiv_1", {
-            debug: true,
-            defaultSeries_type: "column",
-
-            title_label_text: "Gráfica (parámetros pequeños)",
-            yAxis: { label_text: "" },
-            xAxis: {
-                label_text: "",
-                categories: provincias,
-            },
-            series: [
-                {
-                    name: "Estancia media",
-                    points: average_stay,
-                },
-                {
-                    name: "Gasto medio",
-                    points: average_daily_expenditure,
+                    points: rooms,
                 },
                 {
                     name: "Tasa de ocupación por lugar",
@@ -261,81 +189,11 @@
                     points: occupancy_rate_by_weekend_place,
                 },
                 {
-                    name: "Tasa de ocupación de habitación",
+                    name: "Tasa de ocupación por habitación",
                     points: room_occupancy_rate,
                 },
             ],
-        });
-
-        var chart = JSC.chart("chartDiv_3", {
-            debug: true,
-            defaultSeries_type: "column",
-            title_label_text: "Gráfica (todo junto)",
-            yAxis: { label_text: "" },
-            xAxis: {
-                label_text: "",
-                categories: provincias,
-            },
-            series: [
-                {
-                    name: "Turistas",
-                    points: traveler,
-                },
-                {
-                    name: "Pernoctaciones",
-                    points: overnight_stay,
-                },
-                {
-                    name: "Habitaciones",
-                    points: rooms,
-                },
-                {
-                    name: "Estancia media",
-                    points: average_stay,
-                },
-                {
-                    name: "Gasto medio",
-                    points: average_daily_expenditure,
-                },
-                {
-                    name: "Tasa de ocupación por lugar",
-                    points: occupancy_rate_by_place,
-                },
-                {
-                    name: "Tasa de ocupación por lugar de fin de semana",
-                    points: occupancy_rate_by_weekend_place,
-                },
-                {
-                    name: "Tasa de ocupación de habitación",
-                    points: room_occupancy_rate,
-                },
-            ],
-        });
-
-        var chart = JSC.chart("chartDiv_2", {
-            debug: true,
-            defaultSeries_type: "column",
-            title_label_text: "Gráfica (parámetros grandes)",
-            yAxis: { label_text: "" },
-            xAxis: {
-                label_text: "",
-                categories: provincias,
-            },
-            series: [
-                {
-                    name: "Turistas",
-                    points: traveler,
-                },
-                {
-                    name: "Pernoctaciones",
-                    points: overnight_stay,
-                },
-                {
-                    name: "Habitaciones",
-                    points: rooms,
-                },
-            ],
-        });
+        });        
     }
 </script>
 
@@ -347,32 +205,13 @@
     <script src="https://code.jscharting.com/latest/jscharting.js"></script>
 </svelte:head>
 <main>
-    <h2 style="text-align: center; margin-top:100px; font-style:oblique; font-family:initial">
-        Gráfica ocupación de apartamentos, de hoteles y coyuntura turística
-        en Andalucía
-    </h2>
-    <p style="text-align:center">
-        Gráfica con: <a
-            style="text-decoration: none; color:black"
-            href="https://www.highcharts.com/"
-            target="_blank"><u>Highcharts</u></a
-        >
-    </p>
-    <figure class="highcharts-figure">
-        <div id="container" />
-        <p class="highcharts-description" />
-    </figure>
     <div style="margin-left: 30px; margin-right:100px">
-        
+        <h2 style="text-align: center; margin-top:100px; font-style:oblique; font-family:initial">
+            Gráfica ocupación de apartamentos, de hoteles y coyuntura turística
+            en Andalucía
+        </h2>
+        <p style="text-align:center">Source: <a style="text-decoration: none; color:black" href="https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys" target="_blank"><u>API Ocupación de apartamentos</u></a>, <a style="text-decoration: none; color:black" href="https://sos2223-14.appspot.com/api/v2/hotel-occupancy-surveys" target="_blank"><u>API Ocupación de hoteles</u></a> y <a style="text-decoration: none; color:black" href="https://sos2223-14.appspot.com/api/v2/andalusia-tourism-situation-surveys" target="_blank"><u>API Coyuntura turística</u></a></p>
         <p style="text-align:center">Gráfica con: <a style="text-decoration: none; color:black" href="https://jscharting.com/" target="_blank"><u>JSCharting</u></a></p>
-        <div id="chartDiv_1" style="width:1500px; height:500px; margin: 0px auto;"/>
-        <div
-            id="chartDiv_2"
-            style="width:1500px; height:500px; margin: 0px auto; margin-top:40px"
-        />
-        <div
-            id="chartDiv_3"
-            style="width:1500px; height:500px; margin: 0px auto; margin-top:40px"
-        />
+        <div id="chartDiv" style="width:1500px; height:500px; margin: 0px auto;"/>
     </div>
 </main>
