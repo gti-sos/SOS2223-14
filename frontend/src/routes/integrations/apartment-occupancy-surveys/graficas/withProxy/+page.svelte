5a86ca64068b798c009713b36a9d5d8b7e09e523
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-03 14:15:33 +0200

    Cambios

--- frontend/src/routes/integrations/apartment-occupancy-surveys/graficas/withProxy/+page.svelte
+++ frontend/src/routes/integrations/apartment-occupancy-surveys/graficas/withProxy/+page.svelte
@@ -2,7 +2,7 @@
     //@ts-nocheck
     import { onMount } from "svelte";
 
-    let API_compañerx_1 = "https://sos2223-12.appspot.com/agr";
+    let API_compañerx_1 = "https://sos2223-14.appspot.com/agr";
    
     let API_cristina ="https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys";
     let datos = "";
