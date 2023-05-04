1dabbeaea3345d75169f39eb219062c0503ab2bc
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-04 08:03:36 +0200

    Cambios

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -18,7 +18,11 @@ export const nodes = [
 	() => import('./nodes/14'),
 	() => import('./nodes/15'),
 	() => import('./nodes/16'),
-	() => import('./nodes/17')
+	() => import('./nodes/17'),
+	() => import('./nodes/18'),
+	() => import('./nodes/19'),
+	() => import('./nodes/20'),
+	() => import('./nodes/21')
 ];
 
 export const server_loads = [];
@@ -35,11 +39,15 @@ export const dictionary = {
 		"/hotel-occupancy-surveys": [10],
 		"/hotel-occupancy-surveys/[province]/[year]": [11],
 		"/integrations": [12],
-		"/integrations/apartment-occupancy-surveys": [13],
-		"/integrations/apartment-occupancy-surveys/graficas": [14],
-		"/integrations/apartment-occupancy-surveys/graficas/withProxy": [16],
-		"/integrations/apartment-occupancy-surveys/graficas/withoutProxy": [15],
-		"/integrations/apartment-occupancy-surveys/usos": [17]
+		"/integrations/andalusia-tourism-situation-surveys/integrations/withoutProxy": [13],
+		"/integrations/andalusia-tourism-situation-surveys/uses": [14],
+		"/integrations/andalusia-tourism-situation-surveys/uses/IMDb": [15],
+		"/integrations/andalusia-tourism-situation-surveys/uses/PokemonGo": [16],
+		"/integrations/apartment-occupancy-surveys": [17],
+		"/integrations/apartment-occupancy-surveys/graficas": [18],
+		"/integrations/apartment-occupancy-surveys/graficas/withProxy": [20],
+		"/integrations/apartment-occupancy-surveys/graficas/withoutProxy": [19],
+		"/integrations/apartment-occupancy-surveys/usos": [21]
 	};
 
 export const hooks = {
