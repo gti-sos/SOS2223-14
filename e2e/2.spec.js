f647c6b74b7baec3a679d9a259cf67bf6b58f1f8
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-05 12:27:11 +0200

    l12

--- e2e/2.spec.js
+++ e2e/2.spec.js
@@ -1,7 +1,7 @@
 //@ts-check
 import { test, expect } from'@playwright/test';
 
-test('home has the right title', async ({ page }) => {
+/*test('home has the right title', async ({ page }) => {
     await page.goto('http://localhost:8080/');
   
     // Expect a title "to contain" a substring.
@@ -10,8 +10,9 @@ test('home has the right title', async ({ page }) => {
 
   test('navigate to contact list page', async ({ page }) => {
     await page.goto('http://localhost:8080/');
-  
+    
+    //Click the get started link
     await page.getByRole('link' , {name: 'Contacts'}).click();
 
     await expect(page).toHaveTitle(/List contact/);
-  });
+  });*/
