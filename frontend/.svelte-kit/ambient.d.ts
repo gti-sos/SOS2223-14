3bde7d96209b8c9be62424f0ed55d0d65bfed716
Cristina Calderon Garcia <cricalgar1@alum.us.es>
2023-05-12 11:38:08 +0200

    Cambios

--- frontend/.svelte-kit/ambient.d.ts
+++ frontend/.svelte-kit/ambient.d.ts
@@ -42,8 +42,6 @@ declare module '$env/static/private' {
 	export const EDITOR: string;
 	export const EFC_28400: string;
 	export const EXEPATH: string;
-	export const FPS_BROWSER_APP_PROFILE_STRING: string;
-	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GUROBI_HOME: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -173,8 +171,6 @@ declare module '$env/dynamic/private' {
 		EDITOR: string;
 		EFC_28400: string;
 		EXEPATH: string;
-		FPS_BROWSER_APP_PROFILE_STRING: string;
-		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GUROBI_HOME: string;
 		HOME: string;
 		HOMEDRIVE: string;
